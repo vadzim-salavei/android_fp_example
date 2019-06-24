b2342baabe0d44b643a234ed307abd3610deb64c
Vadzim Salavei <vadzim.salavei@bglgroup.co.uk>
2019-06-24 11:58:58 +0300

    Updated Kotlin version.

--- app/src/main/java/com/github/vadzim_salavei/android_fp_example/domain/TodoUseCases.kt
+++ app/src/main/java/com/github/vadzim_salavei/android_fp_example/domain/TodoUseCases.kt
@@ -55,7 +55,7 @@ fun <D : DomainDependencies> deleteTodoByIdUseCase(todoId: Long?): Reader<D, IO<
 
 fun <D : DomainDependencies> updateTodosUseCase(block: (List<Todo>) -> List<Todo>): Reader<D, IO<Unit>> =
     getTodosUseCase<D>().flatMap { todosIo ->
-        setTodosUseCase(todosIo.map(block))
+        setTodosUseCase<D>(todosIo.map(block))
     }
 
 fun <D : DomainDependencies> getTodosUseCase(): Reader<D, IO<List<Todo>>> =
@@ -68,7 +68,7 @@ fun <D : DomainDependencies> getTodosUseCase(): Reader<D, IO<List<Todo>>> =
             preferenceApi
                 .getString(KEY_TODOS, DEFAULT_VALUE_TODOS)
                 .let { todosJson ->
-                    gson.fromJson(todosJson, object : TypeToken<List<Todo>>() {}.type)
+                    gson.fromJson<List<Todo>>(todosJson, object : TypeToken<List<Todo>>() {}.type)
                 }
         }
     }
